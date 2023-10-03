50f5907bb07726fec562bb478addf424e09150f9
Sage Wall <swall@esri.com>
2023-10-03 10:49:00 -0600

    chore(dependencies): update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-	import '@arcgis/core/assets/esri/themes/light/main.css';
-	import '@esri/calcite-components/dist/calcite/calcite.css';
-
 	import type { Widget } from '$lib/types';
 	import * as reactiveUtils from '@arcgis/core/core/reactiveUtils';
 	import type Point from '@arcgis/core/geometry/Point';
 	import type FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+	import type MapView from '@arcgis/core/views/MapView';
+	import type LayerList from '@arcgis/core/widgets/LayerList';
+	import type ListItem from '@arcgis/core/widgets/LayerList/ListItem';
+	import type ListItemPanel from '@arcgis/core/widgets/LayerList/ListItemPanel';
 	import { defineCustomElements as defineMapElements } from '@arcgis/map-components/dist/loader';
 	import { defineCustomElements as defineCalciteElements } from '@esri/calcite-components/dist/loader';
 
 	defineCalciteElements(window, {
-		resourcesUrl: 'https://js.arcgis.com/calcite-components/1.7.0/assets'
+		resourcesUrl: 'https://js.arcgis.com/calcite-components/1.8.0/assets'
 	});
 	defineMapElements();
 
@@ -32,18 +33,16 @@
 		}
 	];
 
-	let arcgisLayerList: HTMLArcgisLayerListElement;
-	let arcgisMap: HTMLArcgisMapElement;
+	let activeWidget = widgets[0];
 	let center: Point;
-	let selectedWidget = widgets[0];
 
-	function handleLayerListReady() {
-		const layerList = arcgisLayerList.widget;
+	function handleLayerListReady(event: { detail: { widget: LayerList } }) {
+		const layerList = event.detail.widget;
 		layerList.listItemCreatedFunction = listItemCreatedFuntion;
 	}
 
-	async function handleViewReady() {
-		const mapView = arcgisMap.view;
+	async function handleViewReady(event: CustomEvent) {
+		const mapView = event.detail.view as MapView;
 		await mapView.when();
 
 		const hazardAreasFeatureLayer = mapView.map.layers.find(
@@ -59,12 +58,12 @@
 		);
 	}
 
-	function listItemCreatedFuntion(event: any) {
+	function listItemCreatedFuntion(event: { item: ListItem }) {
 		const item = event.item;
 		if (item.layer.type != 'group') {
 			item.panel = {
 				content: 'legend'
-			};
+			} as ListItemPanel;
 		}
 	}
 </script>
@@ -74,47 +73,40 @@
 		<calcite-action-bar slot="action-bar">
 			{#each widgets as widget}
 				<calcite-action
-					on:click={() => (selectedWidget = widget)}
-					on:keypress={() => (selectedWidget = widget)}
+					on:click={() => (activeWidget = widget)}
+					on:keypress={() => (activeWidget = widget)}
 					tabindex="0"
 					role="button"
-					active={widget.id === selectedWidget.id ? true : undefined}
+					active={widget.id === activeWidget.id ? true : undefined}
 					icon={widget.icon}
 					text={widget.text}
 				/>
 			{/each}
 		</calcite-action-bar>
 
-		{#if center}
-			<calcite-block open>
-				<div>The view's center</div>
-				<div>
-					{center.longitude.toFixed(2)}° longitude {center.latitude.toFixed(2)}° latitude
-				</div></calcite-block
-			>
-		{/if}
-
-		{#if selectedWidget.id === 'arcgis-layer-list'}
-			<arcgis-layer-list
-				bind:this={arcgisLayerList}
-				on:widgetReady={handleLayerListReady}
-				reference-element="arcgis-map"
-			/>
-		{:else if selectedWidget.id === 'arcgis-legend'}
+		{#if activeWidget.id === 'arcgis-layer-list'}
+			<arcgis-layer-list on:widgetReady={handleLayerListReady} reference-element="arcgis-map" />
+		{:else if activeWidget.id === 'arcgis-legend'}
 			<arcgis-legend reference-element="arcgis-map" />
-		{:else if selectedWidget.id === 'arcgis-editor'}
+		{:else if activeWidget.id === 'arcgis-editor'}
 			<arcgis-editor reference-element="arcgis-map" />
 		{/if}
 	</calcite-shell-panel>
 
-	<arcgis-map
-		bind:this={arcgisMap}
-		on:viewReady={handleViewReady}
-		item-id="459a495fc16d4d4caa35e92e895694c8"
-	/>
+	{#if center}
+		<shell-center-row>
+			<div>
+				Map Center: {center.longitude.toFixed(2)}° longitude {center.latitude.toFixed(2)}° latitude
+			</div></shell-center-row
+		>
+	{/if}
+
+	<arcgis-map on:viewReady={handleViewReady} item-id="459a495fc16d4d4caa35e92e895694c8" />
 </calcite-shell>
 
 <style>
+	@import '@arcgis/core/assets/esri/themes/light/main.css';
+
 	arcgis-map {
 		flex: 1;
 	}
