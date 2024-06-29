e733d787fe656fffeea53763d7afd859dd3dda50
Sage Wall <sagewall@users.noreply.github.com>
2024-06-29 17:42:14 -0600

    chore(deps): update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,10 @@
 <script lang="ts">
 	import type { Widget } from '$lib/types';
-	import type WebMap from '@arcgis/core/WebMap';
-	import * as reactiveUtils from '@arcgis/core/core/reactiveUtils';
+	import '@arcgis/core/assets/esri/themes/light/main.css';
 	import type Point from '@arcgis/core/geometry/Point';
-	import type FeatureLayer from '@arcgis/core/layers/FeatureLayer';
-	import type MapView from '@arcgis/core/views/MapView';
-	import type LayerList from '@arcgis/core/widgets/LayerList';
-	import type ListItem from '@arcgis/core/widgets/LayerList/ListItem';
-	import type ListItemPanel from '@arcgis/core/widgets/LayerList/ListItemPanel';
-	import {
-		defineCustomElements as defineMapElements,
-		type ArcgisLayerListCustomEvent,
-		type ArcgisMapCustomEvent
-	} from '@arcgis/map-components/dist/loader';
-	import { defineCustomElements as defineCalciteElements } from '@esri/calcite-components/dist/loader';
-
-	defineCalciteElements(window, {
-		resourcesUrl: 'https://js.arcgis.com/calcite-components/2.0.0/assets'
-	});
-	defineMapElements();
+	import type { ArcgisMap } from '@arcgis/map-components/dist/components/arcgis-map';
+	import '@esri/calcite-components/dist/calcite/calcite.css';
+	import { onMount } from 'svelte';
 
 	const widgets: Widget[] = [
 		{
@@ -30,117 +16,95 @@
 			icon: 'legend',
 			id: 'arcgis-legend',
 			text: 'Legend'
-		},
-		{
-			icon: 'layers-editable',
-			id: 'arcgis-editor',
-			text: 'Editor'
 		}
 	];
 
+	let arcgisMap: ArcgisMap | null = null;
 	let activeWidget = widgets[0];
 	let center: Point;
-	let mapView: MapView;
+	let mounted = false;
 
 	$: latitude = center?.latitude.toFixed(2);
 	$: longitude = center?.longitude.toFixed(2);
-	$: map = mapView?.map as WebMap;
 
-	function handleLayerListReady(
-		event: ArcgisLayerListCustomEvent<{
-			widget: LayerList;
-		}>
-	) {
-		const layerList = event.detail.widget;
-		layerList.filterPlaceholder = 'Filter layers';
-		layerList.listItemCreatedFunction = listItemCreatedFuntion;
-		layerList.multipleSelectionEnabled = true;
-		layerList.selectionEnabled = true;
-		layerList.visibleElements.collapseButton = true;
-		layerList.visibleElements.closeButton = true;
-		layerList.visibleElements.filter = true;
-		layerList.visibleElements.heading = true;
+	onMount(async () => {
+		await import('@arcgis/map-components/dist/components/arcgis-layer-list');
+		await import('@arcgis/map-components/dist/components/arcgis-legend');
+		await import('@arcgis/map-components/dist/components/arcgis-map');
+		await import('@esri/calcite-components/dist/components/calcite-action');
+		await import('@esri/calcite-components/dist/components/calcite-action-bar');
+		await import('@esri/calcite-components/dist/components/calcite-navigation');
+		await import('@esri/calcite-components/dist/components/calcite-navigation-logo');
+		await import('@esri/calcite-components/dist/components/calcite-shell');
+		await import('@esri/calcite-components/dist/components/calcite-shell-center-row');
+		await import('@esri/calcite-components/dist/components/calcite-shell-panel');
+
+		const { setAssetPath } = await import('@esri/calcite-components/dist/components');
+		setAssetPath('https://js.arcgis.com/calcite-components/2.10.1/assets');
+
+		mounted = true;
+	});
+
+	function handleArcgisViewChange(event: CustomEvent) {
+		center = (event.target as ArcgisMap).center;
 	}
 
-	async function handleViewReady(
-		event: ArcgisMapCustomEvent<{
-			view: __esri.MapView;
-		}>
-	) {
-		mapView = event.target.view;
-
-		const hazardAreasFeatureLayer = mapView.map.layers.find(
-			(layer) => layer.title === 'Hazard Areas'
-		) as FeatureLayer;
-
-		const result = await hazardAreasFeatureLayer.queryFeatures();
-		mapView.goTo(result.features);
-
-		reactiveUtils.watch(
-			() => mapView.center,
-			() => (center = mapView.center)
-		);
-	}
-
-	function listItemCreatedFuntion(event: { item: ListItem }) {
-		const { item } = event;
-		if (item.layer.type != 'group') {
-			item.panel = {
-				content: 'legend'
-			} as ListItemPanel;
-		}
+	function handleArcgisViewReadyChange(event: CustomEvent) {
+		arcgisMap = event.target as ArcgisMap;
 	}
 </script>
 
-<calcite-shell>
-	<calcite-navigation slot="header">
-		<calcite-navigation-logo
-			description={map?.portalItem?.snippet}
-			heading={map?.portalItem?.title}
-			slot="logo"
-			thumbnail={map?.portalItem?.thumbnailUrl}
-			href={map?.portalItem?.itemPageUrl}
-			label="Thumbnail of map"
-		/>
-	</calcite-navigation>
-	<calcite-shell-panel slot="panel-start" position="start">
-		<calcite-action-bar slot="action-bar">
-			{#each widgets as widget}
-				<calcite-action
-					on:click={() => (activeWidget = widget)}
-					on:keypress={() => (activeWidget = widget)}
-					tabindex="0"
-					role="button"
-					active={widget.id === activeWidget.id ? true : undefined}
-					icon={widget.icon}
-					text={widget.text}
-				/>
-			{/each}
-		</calcite-action-bar>
-
-		{#if activeWidget.id === 'arcgis-layer-list'}
-			<arcgis-layer-list on:widgetReady={handleLayerListReady} reference-element="arcgis-map" />
-		{:else if activeWidget.id === 'arcgis-legend'}
-			<arcgis-legend reference-element="arcgis-map" />
-		{:else if activeWidget.id === 'arcgis-editor'}
-			<arcgis-editor reference-element="arcgis-map" />
+{#if mounted}
+	<calcite-shell>
+		<calcite-navigation slot="header">
+			<calcite-navigation-logo
+				description={arcgisMap?.map.portalItem.snippet}
+				heading={arcgisMap?.map.portalItem.title}
+				slot="logo"
+				thumbnail={arcgisMap?.map.portalItem.thumbnailUrl}
+				href={arcgisMap?.map.portalItem.itemPageUrl}
+				label="Thumbnail of map"
+			/>
+		</calcite-navigation>
+		<calcite-shell-panel slot="panel-start" position="start">
+			<calcite-action-bar slot="action-bar">
+				{#each widgets as widget}
+					<calcite-action
+						on:click={() => (activeWidget = widget)}
+						on:keypress={() => (activeWidget = widget)}
+						tabindex="0"
+						role="button"
+						active={widget.id === activeWidget.id ? true : undefined}
+						icon={widget.icon}
+						text={widget.text}
+					/>
+				{/each}
+			</calcite-action-bar>
+			{#if activeWidget.id === 'arcgis-layer-list'}
+				<arcgis-layer-list reference-element="arcgis-map" />
+			{:else if activeWidget.id === 'arcgis-legend'}
+				<arcgis-legend reference-element="arcgis-map" />
+			{/if}
+		</calcite-shell-panel>
+		<arcgis-map
+			item-id="d5dda743788a4b0688fe48f43ae7beb9"
+			on:arcgisViewChange={handleArcgisViewChange}
+			on:arcgisViewReadyChange={handleArcgisViewReadyChange}
+			ref={arcgisMap}
+		></arcgis-map>
+		{#if center}
+			<shell-center-row>
+				<div>
+					Map Center: {longitude}° longitude {latitude}° latitude
+				</div>
+			</shell-center-row>
 		{/if}
-	</calcite-shell-panel>
-
-	{#if center}
-		<shell-center-row>
-			<div>
-				Map Center: {longitude}° longitude {latitude}° latitude
-			</div>
-		</shell-center-row>
-	{/if}
-
-	<arcgis-map on:viewReady={handleViewReady} item-id="459a495fc16d4d4caa35e92e895694c8" />
-</calcite-shell>
+	</calcite-shell>
+{:else}
+	<p>Loading...</p>
+{/if}
 
 <style>
-	@import '@arcgis/core/assets/esri/themes/light/main.css';
-
 	arcgis-map {
 		flex: 1;
 	}
