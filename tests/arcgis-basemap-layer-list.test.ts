6557619c024fc7c2f4b9a53347dd38e3a95b1c8c
Sage Wall <swall@esri.com>
2024-07-18 13:12:17 -0600

    chore: improve tests

--- tests/arcgis-basemap-layer-list.test.ts
+++ tests/arcgis-basemap-layer-list.test.ts
@@ -5,5 +5,5 @@ test('map route has expected arcgis-basemap-layer-list', async ({ page }) => {
 	await page.getByTestId('select-components').click();
 	await page.getByTestId('arcgis-basemap-layer-list-dropdown-item').click();
 	await page.getByTestId('arcgis-basemap-layer-list-dropdown-item-action').click();
-	await expect(page.locator('arcgis-basemap-layer-list')).toBeVisible();
+	await expect(page.getByTitle('Human Geography Dark Detail')).toBeVisible();
 });
--- tests/arcgis-layer-list.test.ts
+++ tests/arcgis-layer-list.test.ts
@@ -5,5 +5,5 @@ test('map route has expected arcgis-layer-list', async ({ page }) => {
 	await page.getByTestId('select-components').click();
 	await page.getByTestId('arcgis-layer-list-dropdown-item').click();
 	await page.getByTestId('arcgis-layer-list-dropdown-item-action').click();
-	await expect(page.locator('arcgis-layer-list')).toBeVisible();
+	await expect(page.getByTitle('Census Tracts')).toBeVisible();
 });
