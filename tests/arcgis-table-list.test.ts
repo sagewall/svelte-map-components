05a6c4fbce2f6cd6694a2642127ba6db4021a1b8
Sage Wall <swall@esri.com>
2024-07-23 10:07:48 -0600

    chore(deps): update

--- tests/arcgis-table-list.test.ts
+++ tests/arcgis-table-list.test.ts
@@ -5,5 +5,5 @@ test('map route has expected table list', async ({ page }) => {
 	await page.getByTestId('select-components').click();
 	await page.getByTestId('arcgis-table-list-dropdown-item').click();
 	await page.getByTestId('arcgis-table-list-dropdown-item-action').click();
-	await expect(page.getByTitle('Chicago public health statistics')).toBeVisible();
+	await expect(page.locator('arcgis-table-list')).toBeVisible();
 });
