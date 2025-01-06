ac4fc91feb6cd996bfcbe93c2a97461a741afd31
Sage Wall <swall@esri.com>
2025-01-06 14:57:29 -0700

    chore(deps): update to 4.32 next

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,9 @@
 
 	onMount(async () => {
 		const { setAssetPath } = await import('@esri/calcite-components/dist/components');
-		setAssetPath('https://js.arcgis.com/calcite-components/2.13.2/assets');
+		setAssetPath(
+			'https://cdn.jsdelivr.net/npm/@esri/calcite-components@3.0.0-next.90/dist/calcite/assets'
+		);
 	});
 </script>
 
