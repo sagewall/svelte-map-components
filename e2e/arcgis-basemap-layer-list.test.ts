5ee165dd254b4043a408efde847461eaaca49d98
Sage Wall <swall@esri.com>
2025-01-28 15:35:53 -0700

    test: update expected titles in e2e tests and increase timeout in config

--- e2e/arcgis-basemap-layer-list.test.ts
+++ e2e/arcgis-basemap-layer-list.test.ts
@@ -5,5 +5,5 @@ test('map route has expected arcgis-basemap-layer-list', async ({ page }) => {
 	await page.getByTestId('select-components').click();
 	await page.getByTestId('arcgis-basemap-layer-list-dropdown-item').click();
 	await page.getByTestId('arcgis-basemap-layer-list-dropdown-item-action').click();
-	await expect(page.getByTitle('Human Geography Dark Detail')).toBeVisible();
+	await expect(page.getByTitle('World Topographic Map')).toBeVisible();
 });
--- e2e/arcgis-layer-list.test.ts
+++ e2e/arcgis-layer-list.test.ts
@@ -5,5 +5,5 @@ test('map route has expected arcgis-layer-list', async ({ page }) => {
 	await page.getByTestId('select-components').click();
 	await page.getByTestId('arcgis-layer-list-dropdown-item').click();
 	await page.getByTestId('arcgis-layer-list-dropdown-item-action').click();
-	await expect(page.getByTitle('Census Tracts')).toBeVisible();
+	await expect(page.getByTitle('Bigfoot sighting')).toBeVisible();
 });
