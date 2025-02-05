8424a092ba139f022a120117bd73cfae9102870d
Sage Wall <swall@esri.com>
2025-02-05 09:01:08 -0700

    chore: update calcite css import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-	import '@esri/calcite-components/dist/calcite/calcite.css';
 	import '@arcgis/core/assets/esri/themes/light/main.css';
 	import '@arcgis/map-components/arcgis-map-components/arcgis-map-components.css';
-	import { onMount } from 'svelte';
+	import '@esri/calcite-components/calcite/calcite.css';
 
 	let { children } = $props();
-
-	onMount(async () => {
-		const { setAssetPath } = await import('@esri/calcite-components/dist/components');
-		setAssetPath(
-			'https://cdn.jsdelivr.net/npm/@esri/calcite-components@3.0.0-next.123/dist/calcite/assets'
-		);
-	});
 </script>
 
 {@render children()}
