a1fe5a408fb5f97b73224ce986dc57d03bde9b56
Sage Wall <swall@esri.com>
2025-02-27 07:57:57 -0700

    chore: add calcite type definitions

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="@arcgis/map-components" />
+/// <reference types="@esri/calcite-components" />
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
