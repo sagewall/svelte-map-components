cc0de5d2c9ae480061342f7e81d35ad59afa1888
Sage Wall <swall@esri.com>
2025-08-11 14:32:11 -0600

    chore: remove core css import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import '@arcgis/core/assets/esri/themes/light/main.css';
 	import '@arcgis/map-components/arcgis-map-components/arcgis-map-components.css';
 	import '@esri/calcite-components/calcite/calcite.css';
 
